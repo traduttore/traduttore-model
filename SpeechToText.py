aaec294677b07963beb6e73662ce66626a391e13
dmohazab <dmohazab@gmail.com>
2022-02-02 13:37:57 -0500

    error

--- SpeechToText.py
+++ SpeechToText.py
@@ -10,7 +10,10 @@ for i in range(5):
     with microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
-    response = recognizer.recognize_google(audio)
+    try:
+        response = recognizer.recognize_google(audio)
+    except:
+        print("I didn't catch that")
 
     print(response)
     time.sleep(2)
