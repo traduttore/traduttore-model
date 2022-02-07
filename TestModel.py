6f787101c33dbd19563c322de69acb1883f98386
dmohazab <dmohazab@gmail.com>
2022-02-06 20:12:08 -0500

    commented code removal

--- TestModel.py
+++ TestModel.py
@@ -1,3 +1,4 @@
+from cv2 import CAP_PROP_FPS
 import cv2
 import numpy as np
 import mediapipe as mp
@@ -33,14 +34,6 @@ def draw_landmarks(image, results):
 
 
 def draw_styled_landmarks(image, results):
-    # # Draw face connections
-    # mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-    #                           mp_drawing.DrawingSpec(
-    #                               color=(80, 110, 10), thickness=1, circle_radius=1),
-    #                           mp_drawing.DrawingSpec(
-    #                               color=(80, 256, 121), thickness=1, circle_radius=1)
-    #                           )
-    # Draw pose connections
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                               mp_drawing.DrawingSpec(
                                   color=(80, 22, 10), thickness=2, circle_radius=4),
@@ -65,8 +58,6 @@ def draw_styled_landmarks(image, results):
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
     ) if results.pose_landmarks else np.zeros(33*4)
-    # face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten(
-    # ) if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten(
     ) if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten(
@@ -103,6 +94,7 @@ def model_predict(data):
     return output_data
 
 cap = cv2.VideoCapture(1)
+cap.set(CAP_PROP_FPS, 10)
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
