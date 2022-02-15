c2f4aea27c297af0400495bafe57d431419b2979
dmohazab <dmohazab@gmail.com>
2022-02-15 11:50:40 -0500

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from run_translation.TestModelComputer import asl_translation
 from run_translation.TextToSpeech import tts
+from run_translation.RunPiModel import rasp_translation
 # from run_translation.SpeechToText import stt
 
 if __name__ == "__main__":
     sentence = asl_translation()
+    # rasp_sentence = rasp_translation()
     tts(sentence)
     # print(stt())
--- run_translation/RunPiModel.py
+++ run_translation/RunPiModel.py
@@ -4,12 +4,13 @@ import mediapipe as mp
 # from tensorflow.keras.models import load_model
 from timeit import default_timer as timer
 import tflite_runtime.interpreter as tflite
+# import pyttsx3
 import time
 from gestures import actions
 from CvFpsCalc import CvFpsCalc
 
+
 mp_holistic = mp.solutions.holistic  # Holistic model
-mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
 def mediapipe_detection(image, model):
     # COLOR CONVERSION BGR 2 RGB
@@ -19,39 +20,6 @@ def mediapipe_detection(image, model):
     image.flags.writeable = True                   # Image is now writeable
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGBß 2 BGR
     return image, results
-
-
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.pose_landmarks,
-                              mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
-                              mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
-                              mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
-
-
-def draw_styled_landmarks(image, results):
-    # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 22, 10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(80, 44, 121), thickness=2, circle_radius=2)
-                              )
-    # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(121, 22, 76), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(121, 44, 250), thickness=2, circle_radius=2)
-                              )
-    # Draw right hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(
-                                  color=(245, 117, 66), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(
-                                  color=(245, 66, 230), thickness=2, circle_radius=2)
-                              )
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten(
@@ -68,6 +36,7 @@ def prob_viz(res, actions, input_frame, colors):
     for num, prob in enumerate(res):
         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
     return output_frame
 
 # 1. New detection variables
@@ -75,7 +44,9 @@ def prob_viz(res, actions, input_frame, colors):
 threshold = 0.9
 
 colors = [(245, 117, 16)]*(actions.size)
+hand_in_screen = True
 
+# model = load_model('action')
 interpreter = tflite.Interpreter(model_path='model.tflite')
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
@@ -88,15 +59,15 @@ def model_predict(data):
     output_data = interpreter.get_tensor(output_details[0]['index'])
     return output_data
 
-def asl_translation():
+def rasp_translation():
     sequence = []
     sentence = []
     start = None
     cap = cv2.VideoCapture(1)
-    cvFpsCalc = CvFpsCalc(buffer_len=10)
-    # Set mediapipe model 
     holistic_def = mp_holistic.Holistic(
         min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    # Set mediapipe model 
+    cvFpsCalc = CvFpsCalc(buffer_len=10)
     while cap.isOpened():
         display_fps = cvFpsCalc.get()
         print(display_fps)
@@ -106,14 +77,9 @@ def asl_translation():
         # Make detections
         image, results = mediapipe_detection(frame, holistic_def)
         
-        # Draw landmarks
-        draw_styled_landmarks(image, results)
-        
         # 2. Prediction logic
         keypoints = extract_keypoints(results)
 
-        # sequence.insert(0,keypoints)
-        # sequence = sequence[:30]
         sequence.append(keypoints)
         sequence = sequence[-30:]
         
@@ -121,12 +87,13 @@ def asl_translation():
             res = model_predict(np.expand_dims(sequence, axis=0))[0]
             
         # 3. Viz logic
-            print(res[np.argmax(res)])
             if res[np.argmax(res)] > threshold: 
                 if actions[np.argmax(res)] == '-':
                     if start:
                         elapsed = timer() - start
                         if elapsed>10:
+                            cap.release()
+                            cv2.destroyAllWindows()
                             return (' '.join(sentence)).replace('-', ' ')
                     else:
                         start = timer()
@@ -141,11 +108,7 @@ def asl_translation():
 
             if len(sentence) > 7: 
                 sentence = []
-
-            # Viz probabilities
-            image = prob_viz(res, actions, image, colors)
             
-        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
         output_sentence = (' '.join(sentence)).replace('-', ' ')
         print(output_sentence)
         try:
@@ -155,12 +118,17 @@ def asl_translation():
                 and not (0.15 < y_coordinate_left_hand < 0.9) \
                 and not (0.15 < y_coordinate_right_hand < 0.9):
                 output_sentence = "Please make sure the hand is within frame."
+                hand_in_screen = False
+            else:
+                hand_in_screen = True
         except:
             pass
-        cv2.putText(image, output_sentence, (3,30), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        # Show to screen
-        cv2.imshow('OpenCV Feed', image)
+        if not hand_in_screen:
+            cv2.putText(image, output_sentence, (3,30), 
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow('OpenCV Feed', image)
+        else:
+            cv2.destroyAllWindows()
 
         # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
@@ -168,7 +136,6 @@ def asl_translation():
     cap.release()
     cv2.destroyAllWindows()
 
-
 if __name__ == '__main__':
-    sentence = asl_translation()
+    sentence = rasp_translation()
     print(sentence)
