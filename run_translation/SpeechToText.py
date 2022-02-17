ed64ddb5b56a6721f325e3c3db0a640db3a029ce
dmohazab <dmohazab@gmail.com>
2022-02-17 11:23:09 -0500

    test

--- run_translation/SpeechToText.py
+++ run_translation/SpeechToText.py
@@ -15,3 +15,8 @@ def stt():
     # except:
     #     response = "I didn't catch that"
     return response
+
+print(stt())
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone with name \"{1}\" found for Microphone(device_index={0})".format(
+        index, name))
