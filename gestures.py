60411ee48031459c25689d8803251219bbf8639f
dmohazab <dmohazab@gmail.com>
2022-02-21 16:43:19 -0500

    pretty good

--- gestures.py
+++ gestures.py
@@ -1,3 +1,3 @@
 import numpy as np
 
-actions = np.array(['hello', 'thank-you', 'coffee', '-', 'tea', 'please', 'medium', 'no'])
+actions = np.array(['hello', 'thank-you', 'coffee', '-', 'tea', 'please', 'medium', 'no', 'i'])
