cf0f03f7bac649788f649f4af264626b52b4731e
dmohazab <dmohazab@gmail.com>
2022-03-04 14:30:19 -0500

    training and letters

--- gestures.py
+++ gestures.py
@@ -2,3 +2,5 @@ import numpy as np
 
 actions = np.array(['hello', 'thank-you', 'coffee', '-', 'tea', 'please', 'medium', 'no', 'muffin', 'small', 'large', 'i', 'a', 'can', 'get'])
 # actions = np.array(['hello', 'thank-you', 'coffee', '-', 'tea', 'please', 'medium', 'no', 'muffin', 'i', 'a', 'can', 'get'])
+letters = np.array(['-', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
