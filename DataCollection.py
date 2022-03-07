a98ce81ef788f336b5723d97c67b15e3f8426ac2
dmohazab <dmohazab@gmail.com>
2022-03-07 15:52:54 -0500

    change

--- DataCollection.py
+++ DataCollection.py
@@ -11,15 +11,9 @@ mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 DATA_PATH = os.path.join('MP_Data')
 
 # Constants
-<<<<<<< HEAD
 WEBCAM_ID = 1
 TARGET_WORD = '-'
 VIDEOS_TO_COLLECT = 30
-=======
-WEBCAM_ID = 0
-TARGET_WORD = 'no'
-VIDEOS_TO_COLLECT = 60
->>>>>>> f2fb7dcb75539987b225c564fcc6079ed3442673
 folder_path = os.path.join(DATA_PATH, TARGET_WORD)
 try:
     len_data = len(os.listdir(folder_path))
