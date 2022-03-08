3dda398607ee48d865a529fe62d5166999d12113
dmohazab <dmohazab@gmail.com>
2022-03-08 12:09:45 -0500

    testing

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from run_translation.TestModelComputer import asl_translation
 from run_translation.TextToSpeech import tts
-from run_translation.RunPiModel import rasp_translation
+from run_translation.RunPiModelTesting import rasp_translation
 from run_translation.TestModelComputerLetters import asl_translation_letters
 # from run_translation.SpeechToText import stt
 
 if __name__ == "__main__":
     # sentence = asl_translation(CAM_ID=0)
-    sentence = asl_translation_letters(CAM_ID=0)
-    # rasp_sentence = rasp_translation()
+    # sentence = asl_translation_letters(CAM_ID=0)
+    rasp_sentence = rasp_translation(CAM_ID=0)
     # tts(sentence)
     # print(stt())
