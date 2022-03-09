d7366f1a8d82312e4106668cea532a39a3f3d13b
dmohazab <dmohazab@gmail.com>
2022-03-09 15:29:25 -0500

    model trained

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from run_translation.TestModelComputer import asl_translation
 from run_translation.TextToSpeech import tts
 from run_translation.RunPiModelStream import rasp_translation
 # from run_translation.RunPiModelTesting import rasp_translation
-# from run_translation.TestModelComputerLetters import asl_translation_letters
+from run_translation.TestModelComputerLetters import asl_translation_letters
 # from run_translation.SpeechToText import stt
 
 if __name__ == "__main__":
-    sentence = rasp_translation()
-    # sentence = asl_translation_letters(CAM_ID=0)
+    # sentence = rasp_translation()
+    sentence = asl_translation_letters(CAM_ID=1)
     # rasp_sentence = rasp_translation()
     # tts(sentence)
     # print(stt())
