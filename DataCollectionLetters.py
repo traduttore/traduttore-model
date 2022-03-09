d04d8c18ae927e3dd6188a1ceae21cea50d231dc
dmohazab <dmohazab@gmail.com>
2022-03-09 14:57:46 -0500

    letters

--- DataCollectionLetters.py
+++ DataCollectionLetters.py
@@ -12,7 +12,7 @@ DATA_PATH = os.path.join('LETTER_Data')
 
 # Constants
 WEBCAM_ID = 1
-TARGET_WORD = 's'
+TARGET_WORD = 'w'
 VIDEOS_TO_COLLECT = 30
 folder_path = os.path.join(DATA_PATH, TARGET_WORD)
 try:
